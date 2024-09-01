ed0179208a760d250f5fd13a521f005fba5f3f7e
Андрей <40599394+Rige214@users.noreply.github.com>
2024-09-01 22:03:50 +0300

    Update randomFunc.py
    UPD 01-09-2024. Check README

--- randomFunc.py
+++ randomFunc.py
@@ -1,40 +1,42 @@
 import sqlite3
 import random
+import datetime
 
 
-connection = sqlite3.connect('santa.db')
+DB = 'santa2.db'
+
+connection = sqlite3.connect(DB)
 cursor = connection.cursor()
 cursor.execute("SELECT * FROM Users ")
 res = cursor.fetchall()
 result_list = [list(row) for row in res]
 connection.commit()
 connection.close()
+kolvo = len(result_list)
 
-print(result_list)
-
-connection = sqlite3.connect('santa.db')
-cursor = connection.cursor()
-pairs = {}
-
-colvo = len(result_list)
-newPairs = random.sample(range(colvo), colvo)
-print(colvo)
-for p in range(0, colvo):
-    userId = result_list[p][1]
-    userName = result_list[p][2]
-    userWish = result_list[p][3]
-    userPairs = result_list[p][4]
-    if(userPairs is None):
-            ppp = newPairs[p]
-            connection = sqlite3.connect('santa.db')
-            cursor = connection.cursor()
-            cursor.execute("UPDATE Users SET pair = ? WHERE telegramId = ?", (str(ppp), userId))
-            connection.commit()
-            connection.close()
-            print(ppp)
-    else:
-        print('Пара существует')
-        break
-p+1
-print(newPairs)
-print(userId, userName, userWish, userPairs)
+def func():
+    newPairs = random.sample(range(kolvo), kolvo)
+    for p in range(0, kolvo):
+        bdId = result_list[p][0]
+        userId = result_list[p][1]
+        userPairs = result_list[p][4]
+        if(userPairs is None):
+                    if (bdId != newPairs[p]):
+                        ppp = newPairs[p]
+                        connection = sqlite3.connect(DB)
+                        cursor = connection.cursor()
+                        cursor.execute("UPDATE Users SET pair = ? WHERE telegramId = ?", (str(ppp), userId))
+                        connection.commit()
+                        connection.close()      
+                    else:
+                        print('Произошла ошибка создания пары. Пользователь не должен являться парой сам себе. Пожалуйста, повторите попытку')
+                        func()
+        else:
+            print('Пары уже существуют')
+            break
+    time_curt = datetime.datetime.now()
+    time_stamp = time_curt.timestamp()
+    date_time = datetime.datetime.fromtimestamp(time_stamp)
+    print(date_time)
+    
+func()
